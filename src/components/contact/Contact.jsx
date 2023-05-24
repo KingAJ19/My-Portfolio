f1b5dc315725516029243766b74dcda691fe9425
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-05-24 10:17:35 +0200

    comments

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -31,9 +31,6 @@ const Contact = () => {
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
-      {/* <h5>
-        Contact me on my email
-      </h5> */}
       <h2>Contact Me</h2>
       <div className="container contact__container">
         <div className="contact__options">
