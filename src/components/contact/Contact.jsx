cd96b9bf8e049c3dd9db90fabc1480fc2b743637
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-05-29 05:12:13 +0200

    no

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from 'react'; 
 import emailjs from '@emailjs/browser';
 import { MdOutlineEmail } from 'react-icons/md'; 
 import { BsWhatsapp } from 'react-icons/bs'
