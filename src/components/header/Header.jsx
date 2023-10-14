32c96f93d2dbfc6ed40bcd6e8979d276fbdea45f
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-10-14 09:36:34 +0200

    added a comma

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <header id="home">
       <div className="container header__container">
-        <h5>Hello I'm</h5>
+        <h5>Hello, I'm</h5>
         <h1>Anele Sokwane</h1>
         <h5 className="text-light">Junior Web Developer</h5>
         <CTA />
