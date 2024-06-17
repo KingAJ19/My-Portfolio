7ebfb978977b4403408d4bb300f5198ef573a52e
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2024-06-17 08:46:19 +0200

    Update HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -9,6 +9,8 @@ const HeaderSocials = () => {
       <a href="https://www.linkedin.com/in/anele-sokwane/" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
       <a href="https://github.com/KingAJ19" target="_blank" rel="noreferrer" ><FaGithub /></a>
       <a href="https://www.youtube.com/@KingAJ19" target="_blank" rel="noreferrer" ><BsYoutube /></a>
+      <a href="https://www.youtube.com/@AneleSokwane" target="_blank" rel="noreferrer" ><BsYoutube /></a>
+      <a href="https://www.youtube.com/@liverpoolfan760" target="_blank" rel="noreferrer" ><BsYoutube /></a>
     </div>
   )
 }
